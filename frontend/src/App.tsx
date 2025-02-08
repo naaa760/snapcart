1307a46453103c67efcc20d7feec25c20dfff92e
nehhhhha6 <n0370525@gmail.com>
2025-02-08 17:12:40 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-
 import Navbar from "./components/Navbar";
+import "./index.css";
+
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
+import { Routes, Route } from "react-router-dom";
 import { useThemeStore } from "./store/useThemeStore";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { theme } = useThemeStore() as { theme: string };
@@ -16,10 +18,12 @@ function App() {
       data-theme={theme}
     >
       <Navbar />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+
       <Toaster />
     </div>
   );
