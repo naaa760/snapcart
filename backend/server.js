05b38d9cf7150aabb8d907f8a00891bf01a8b1eb
nehhhhha6 <n0370525@gmail.com>
2025-02-08 08:10:28 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,26 @@ app.use(morgan("dev"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log("server is running on port" + PORT);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server is running on port" + PORT);
+  });
 });
